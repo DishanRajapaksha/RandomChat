d331aadea81b7e619fff673b1891fd9fa848b65d
Dishan Rajapaksha <dsrajapaksha@outlook.com>
2017-06-04 12:34:40 +0530

    Changes

--- app.js
+++ app.js
@@ -1,89 +1,84 @@
-var cfenv = require('cfenv');
-var appEnv = cfenv.getAppEnv();
-
-var express=require('express');
+var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(appEnv.port);
-console.log(appEnv.port);
+server.listen('3000');
 app.use(express.static('public'));
-
 
 lonelyClient = {};
 allClients = {};
 
 io.on('connection', function (socket) {
     io.to(socket.id).emit('userCount', Object.keys(allClients).length);
-    console.log(socket.id,'just came to website')
-    
-    socket.on('disconnect', function() {
-        if(allClients[socket.id]){
-            if(lonelyClient.id == socket.id){
-                    lonelyClient = {};
-            }else{
-                if(allClients[allClients[socket.id].partner]){
+    console.log(socket.id, 'just came to website')
+
+    socket.on('disconnect', function () {
+        if (allClients[socket.id]) {
+            if (lonelyClient.id == socket.id) {
+                lonelyClient = {};
+            } else {
+                if (allClients[allClients[socket.id].partner]) {
                     io.to(allClients[socket.id].partner).emit('aborted');
                 }
             }
-            
+
             delete allClients[socket.id];
 
             console.log(socket.username, 'disconnected!');
             console.log(Object.keys(allClients).length + ' users online');
-        }else{
+        } else {
             console.log('A user that never registered left')
         }
-        
+
         io.sockets.emit('userCount', Object.keys(allClients).length);
 
     });
-        
-    socket.on('delete', function(){
-        if(lonelyClient.id == socket.id){
+
+    socket.on('delete', function () {
+        if (lonelyClient.id == socket.id) {
             lonelyClient = {};
         }
-        if(allClients[allClients[socket.id].partner]){
+        if (allClients[allClients[socket.id].partner]) {
             io.to(allClients[socket.id].partner).emit('aborted');
         }
         delete allClients[socket.id];
         console.log(socket.username, 'left')
         console.log(Object.keys(allClients).length + ' users online now')
     });
-    
-    socket.on('new user', function(data){
-        
-        if(!data || data==""){
+
+    socket.on('new user', function (data) {
+
+        if (!data || data == "") {
             console.log('error: no username');
             return;
         }
-        if(data.length>30){
-            data = data.substring(0,29)
+        if (data.length > 30) {
+            data = data.substring(0, 29)
         }
         socket.username = data;
         allClients[socket.id] = socket;
 
-        console.log('new user', data); 
-                
-        if(lonelyClient.id){
+        console.log('new user', data);
+
+        if (lonelyClient.id) {
             console.log(lonelyClient.username, 'matchar nu med', socket.username);
             socket.partner = lonelyClient.id;
             allClients[lonelyClient.id].partner = socket.id;
             allClients[socket.id].partner = lonelyClient.id;
-            
+
             io.to(lonelyClient.id).emit('match', {
-                username:data,
-                id:socket.id
+                username: data,
+                id: socket.id
             });
             io.to(socket.id).emit('match', {
-                username:lonelyClient.username,
-                id:lonelyClient.id
+                username: lonelyClient.username,
+                id: lonelyClient.id
             });
-            
+
             lonelyClient = {};
 
-        }else{
+        } else {
             console.log(socket.username, 'är ensam här');
             lonelyClient.username = data;
             lonelyClient.id = socket.id;
@@ -93,44 +88,43 @@ io.on('connection', function (socket) {
         io.sockets.emit('userCount', Object.keys(allClients).length);
 
     });
-    
-    socket.on('new message', function(data){
+
+    socket.on('new message', function (data) {
         console.log(data)
-        if(allClients[data.partnerID]){
-              
+        if (allClients[data.partnerID]) {
+
             io.to(data.partnerID).emit('incoming message', {
-                userID:socket.id,
-                user:socket.username,
-                message:data.message
+                userID: socket.id,
+                user: socket.username,
+                message: data.message
             });
             io.to(socket.id).emit('incoming message', {
-                userID:socket.id,
-                user:socket.username,
-                message:data.message
+                userID: socket.id,
+                user: socket.username,
+                message: data.message
             });
             io.to(data.partnerID).emit('stop typing', {
                 username: socket.username
             });
 
-        }else{
-           io.to(socket.id).emit('aborted');
+        } else {
+            io.to(socket.id).emit('aborted');
         }
     })
-    
-          // when the client emits 'typing', we broadcast it to others
-      socket.on('typing', function (data) {
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', function (data) {
         io.to(data).emit('typing', {
-          username: socket.username
+            username: socket.username
         });
-      });
+    });
 
-      // when the client emits 'stop typing', we broadcast it to others
-      socket.on('stop typing', function (data) {
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', function (data) {
         io.to(data).emit('stop typing', {
-          username: socket.username
+            username: socket.username
         });
-      });
+    });
 
-    
+
 });
-
--- public/app.config.js
+++ public/app.config.js
@@ -1,192 +1,192 @@
-var app = angular.module('IBMfinder', ['ngRoute']);
+var app = angular.module('Finder', ['ngRoute']);
 
-app.config(['$routeProvider', 
-  function($routeProvider, settings) {
-    $routeProvider
-      .when('/main', {
-        templateUrl: 'welcome.html',
-        controller: 'welcomeCtrl',
-      })
-      .when('/find', {
-        templateUrl: 'find.html',
-        controller: 'findCtrl',
-      })
-      .when('/chat', {
-        templateUrl: 'chat.html',
-        controller: 'chatCtrl',
-      })
+app.config(['$routeProvider',
+    function ($routeProvider, settings) {
+        $routeProvider
+            .when('/main', {
+                templateUrl: 'welcome.html',
+                controller: 'welcomeCtrl',
+            })
+            .when('/find', {
+                templateUrl: 'find.html',
+                controller: 'findCtrl',
+            })
+            .when('/chat', {
+                templateUrl: 'chat.html',
+                controller: 'chatCtrl',
+            })
 
-      .otherwise({
-        templateUrl: 'welcome.html',
-        controller: 'welcomeCtrl',
-      })
-}])
+            .otherwise({
+                templateUrl: 'welcome.html',
+                controller: 'welcomeCtrl',
+            })
+    }])
 
-app.controller('userCount', ['$scope', 'socket', function($scope, socket){
-    socket.on('userCount', function(amount){
+app.controller('userCount', ['$scope', 'socket', function ($scope, socket) {
+    socket.on('userCount', function (amount) {
         $scope.online = amount;
-        
+
     })
 }]);
 
-app.controller('welcomeCtrl', ['$scope', '$location', 'settings', 'socket', function($scope, $location, settings, socket){
+app.controller('welcomeCtrl', ['$scope', '$location', 'settings', 'socket', function ($scope, $location, settings, socket) {
     $scope.users = 13;
-    if(settings.getUsername()!==""){
+    if (settings.getUsername() !== "") {
         socket.emit('delete');
         settings.reset();
     }
-    
-    $scope.enter = function(){
+
+    $scope.enter = function () {
         settings.setUsername($scope.name);
         $location.path('/find');
     }
 }]);
 
-app.controller('findCtrl', ['$scope', '$location', 'settings', 'socket', '$rootScope', function($scope, $location, settings, socket, $rootScope){
+app.controller('findCtrl', ['$scope', '$location', 'settings', 'socket', '$rootScope', function ($scope, $location, settings, socket, $rootScope) {
     $scope.username = settings.getUsername();
 
-    if(!$scope.username || $scope.username == ""){
+    if (!$scope.username || $scope.username == "") {
         location.href = "index.html";
     }
-    if(settings.exists){
+    if (settings.exists) {
         socket.emit('delete');
         location.href = "index.html";
-        
+
     }
 
     $scope.chatlog = [];
 
-    if(!settings.exists){
+    if (!settings.exists) {
         var username = $scope.username;
         settings.setExists(true);
-        
-        socket.emit('new user', username );
+
+        socket.emit('new user', username);
     };
-    
+
     socket.on('match', function (data) {
         settings.setPartner(data['username'], data['id']);
-         $location.path('/chat');
+        $location.path('/chat');
     });
 
 }]);
 
 
-app.controller('chatCtrl', ['$scope', '$location', 'settings', 'socket', '$rootScope', '$timeout', '$window', '$interval', function($scope, $location, settings, socket, $rootScope, $timeout, $window, $interval){
+app.controller('chatCtrl', ['$scope', '$location', 'settings', 'socket', '$rootScope', '$timeout', '$window', '$interval', function ($scope, $location, settings, socket, $rootScope, $timeout, $window, $interval) {
     var typing = false;
     var focus = true;
     var titleTimer;
-    var onFocus = function(){
+    var onFocus = function () {
         focus = true;
         $interval.cancel(titleTimer);
         document.title = 'Findr';
     }
-    var onBlur = function(){
+    var onBlur = function () {
         focus = false;
     }
     $window.onfocus = onFocus;
-    $window.onblur = onBlur;   
+    $window.onblur = onBlur;
 
     $scope.username = settings.getUsername();
     $scope.partnerTyping = false;
-    
-    if(!$scope.username || $scope.username == ""){
+
+    if (!$scope.username || $scope.username == "") {
         location.href = "index.html";
     }
 
     $scope.chatlog = [];
 
-    if(!settings.exists){
+    if (!settings.exists) {
         var username = $scope.username;
         settings.setExists(true);
-        
-        socket.emit('new user', username );
+
+        socket.emit('new user', username);
     };
-    
-    socket.on('incoming message', function(data){
-        if($scope.chatlog[$scope.chatlog.length-1]){
-            if($scope.chatlog[$scope.chatlog.length-1].sentby == data.userID){
-                $scope.chatlog[ $scope.chatlog.length] = {
-                    sentby:data.userID,
+
+    socket.on('incoming message', function (data) {
+        if ($scope.chatlog[$scope.chatlog.length - 1]) {
+            if ($scope.chatlog[$scope.chatlog.length - 1].sentby == data.userID) {
+                $scope.chatlog[$scope.chatlog.length] = {
+                    sentby: data.userID,
                     chatusername: '',
                     chatmessage: data.message
                 }
-            }else{
-                $scope.chatlog[ $scope.chatlog.length] = {
-                    sentby:data.userID,
+            } else {
+                $scope.chatlog[$scope.chatlog.length] = {
+                    sentby: data.userID,
                     chatusername: data.user + ": ",
                     chatmessage: data.message
                 }
 
             }
-        }else{
-            $scope.chatlog[ $scope.chatlog.length] = {
-                sentby:data.userID,
+        } else {
+            $scope.chatlog[$scope.chatlog.length] = {
+                sentby: data.userID,
                 chatusername: data.user + ": ",
                 chatmessage: data.message
             }
         }
 
-        if(!focus){
+        if (!focus) {
             document.title = 'Nytt meddelande!';
-            
+
             $interval.cancel(titleTimer);
-            titleTimer = $interval(function(){
-                if(document.title == 'Nytt meddelande!'){
+            titleTimer = $interval(function () {
+                if (document.title == 'Nytt meddelande!') {
                     document.title = 'Findr';
-                }else{
+                } else {
                     document.title = 'Nytt meddelande!';
                 }
             }, 1000)
         }
 
     });
-    
-    socket.on('aborted',  function(data){
+
+    socket.on('aborted', function (data) {
         alert('Your partner left, sorry!');
         socket.emit('delete');
         settings.reset();
         location.href = "index.html";
     })
-    
-    $scope.typing = function(){
-        if(!typing){
+
+    $scope.typing = function () {
+        if (!typing) {
             socket.emit('typing', settings.getID());
             typing = true;
-        var stop = $timeout(function() {
-            typing = false;
-            socket.emit('stop typing', settings.getID());
-        }, 2000);
+            var stop = $timeout(function () {
+                typing = false;
+                socket.emit('stop typing', settings.getID());
+            }, 2000);
 
         }
 
 
     }
-    
-    socket.on('typing', function(data){
+
+    socket.on('typing', function (data) {
         $scope.partnerTyping = true;
         $('#chatbox').scrollTop(10000);
 
     })
-    
-    socket.on('stop typing', function(data){
+
+    socket.on('stop typing', function (data) {
         $scope.partnerTyping = false;
         $('#chatbox').scrollTop(10000);
 
     })
 
 
-    $scope.sendMessage = function(){
-        if($scope.message==""){
-            
-        }else{
-            socket.emit( 'new message', {
-                message:$scope.message, 
-                partner:$scope.partner,
+    $scope.sendMessage = function () {
+        if ($scope.message == "") {
+
+        } else {
+            socket.emit('new message', {
+                message: $scope.message,
+                partner: $scope.partner,
                 partnerID: settings.getID()
             });
         }
-        
-        $scope.message = "";        
+
+        $scope.message = "";
     }
 
     $scope.partner = settings.getPartner();
@@ -194,7 +194,7 @@ app.controller('chatCtrl', ['$scope', '$location', 'settings', 'socket', '$rootS
 }]);
 
 
-app.service('settings', function() {
+app.service('settings', function () {
     this.exists = false;
     this.username = "";
     this.partner = "";
@@ -202,32 +202,32 @@ app.service('settings', function() {
     this.userdata = {}
 
 
-    this.setExists = function(bool){
+    this.setExists = function (bool) {
         this.exists = bool;
     }
-    this.setUsername = function(uname){
+    this.setUsername = function (uname) {
         this.username = uname;
     }
-    this.getUsername = function(){
-        return(this.username);
+    this.getUsername = function () {
+        return (this.username);
     }
-    this.setUserID = function(id){
+    this.setUserID = function (id) {
         this.userdata.id = id;
     }
-    this.getuserdata = function(){
-        return(this.userdata);
+    this.getuserdata = function () {
+        return (this.userdata);
     }
-    this.setPartner = function(uname, id){
+    this.setPartner = function (uname, id) {
         this.partner = uname;
         this.partnerID = id;
     }
-    this.getPartner = function(){
-        return(this.partner);
+    this.getPartner = function () {
+        return (this.partner);
     }
-    this.getID = function(){
-        return(this.partnerID);
+    this.getID = function () {
+        return (this.partnerID);
     }
-    this.reset = function(){
+    this.reset = function () {
         this.exists = false;
         this.username = "";
         this.partner = "";
@@ -237,38 +237,38 @@ app.service('settings', function() {
 });
 
 app.factory('socket', function ($rootScope) {
-  var socket = io.connect();
-  return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {  
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      })
-    },
-    disconnect: function(id){
-        socket.disconnect(id);
-    }
-  };
+    var socket = io.connect();
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        },
+        disconnect: function (id) {
+            socket.disconnect(id);
+        }
+    };
 });
 
 
 app.directive('myEnter', function () {
     return function (scope, element, attrs) {
         element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
+            if (event.which === 13) {
+                scope.$apply(function () {
                     scope.$eval(attrs.myEnter);
                 });
 
@@ -279,18 +279,17 @@ app.directive('myEnter', function () {
 });
 
 app.directive('schrollBottom', function () {
-  return {
-    scope: {
-      schrollBottom: "="
-    },
-    link: function (scope, element) {
-      scope.$watchCollection('schrollBottom', function (newValue) {
-        if (newValue)
-        {
-          $(element).scrollTop(100000);
+    return {
+        scope: {
+            schrollBottom: "="
+        },
+        link: function (scope, element) {
+            scope.$watchCollection('schrollBottom', function (newValue) {
+                if (newValue) {
+                    $(element).scrollTop(100000);
+                }
+            });
         }
-      });
     }
-  }
 })
 
